f33953d10b6184933d0241ec383e7a3899474999
Mo <mowoodsiii@gmail.com>
2018-04-27 17:50:04 +0000

    Started names.py script; added Mo, Brianne, Dan, and Kai's names

---
+++ names.py
@@ -0,0 +1,16 @@
+# Make the script print your name (and everyone else's name) any way you'd like!
+
+print("Mo Woods")
+
+string = "Brianne"
+print(string+" Willett")
+
+string = "Dan Godrick"
+for letter in string:
+    print(letter)
+
+first = "Kai"
+last = "Amey"
+print("First name: %s, Last name: %s" % (first,last))
+
+
